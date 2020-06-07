88731255a450bec0d0477097df78df7e8d4f52ae
Julia <yulenkavip@gmail.com>
2020-06-07 16:13:13 +0300

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/core/header/header.component';
+import { FooterComponent } from './components/core/footer/footer.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { TestComponent } from './components/home-page/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
