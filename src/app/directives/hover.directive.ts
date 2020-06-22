3a2b05d2971602b0bc22920f77d2c33b2a23b316
Julia <yulenkavip@gmail.com>
2020-06-22 15:27:54 +0300

    hover host binding

--- src/app/directives/hover.directive.ts
+++ src/app/directives/hover.directive.ts
@@ -1,22 +1,28 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
 })
 export class HoverDirective {
-  @Input() dHover: { border: string, fontSize: string};
+  @Input() dHover: { borderColor: string, fontSize: string};
+  @HostBinding('style.fontSize') elFontSize = null;
+  @HostBinding('style.borderColor') elBorder = null;
 
   constructor(private elRef: ElementRef,
               private renderer: Renderer2) { }
 
   @HostListener('mouseenter') onEnter() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'border', this.dHover.border);
-    this.renderer.setStyle(this.elRef.nativeElement, 'fontSize', this.dHover.fontSize)
+    this.elFontSize = this.dHover.fontSize;
+    this.elBorder = this.dHover.borderColor;
+    // this.renderer.setStyle(this.elRef.nativeElement, 'border', this.dHover.border);
+    // this.renderer.setStyle(this.elRef.nativeElement, 'fontSize', this.dHover.fontSize);
   }
 
   @HostListener('mouseleave') onLeave() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'border', null);
-    this.renderer.setStyle(this.elRef.nativeElement, 'fontSize', null)
+    this.elFontSize = null;
+    this.elBorder = null;
+    // this.renderer.setStyle(this.elRef.nativeElement, 'border', null);
+    // this.renderer.setStyle(this.elRef.nativeElement, 'fontSize', null);
   }
 
 }
