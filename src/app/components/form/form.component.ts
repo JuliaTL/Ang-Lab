11da3e836fa2b85ae913d48691f3d445f1a2a277
Julia <yulenkavip@gmail.com>
2020-07-16 19:19:40 +0300

    Form Template-Driven

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from "@angular/forms";
 
 @Component({
@@ -6,15 +6,26 @@ import { NgForm } from "@angular/forms";
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
   @ViewChild('form') form: NgForm;
+  defaultQuestion = 'car';
+  submitted = false;
 
-  constructor() { }
+  user = {
+    name: '',
+    email: '',
+    password: '',
+    question: '',
+  };
 
-  ngOnInit(): void {
-  }
-
-  onSubmit(form) {
-console.log(form)
+  onSubmit() {
+    this.submitted = true;
+    this.user.name = this.form.value.username;
+    this.user.email = this.form.value.email;
+    this.user.password = this.form.value.password;
+    this.user.question = this.form.value.question;
+    setTimeout(()=>
+      this.form.reset(),
+      3000)
   }
 }
