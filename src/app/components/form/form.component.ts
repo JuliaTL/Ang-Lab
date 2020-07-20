2556f18226676188662d04e503caa149160b39b4
Julia <yulenkavip@gmail.com>
2020-07-20 16:23:42 +0300

    t11

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -20,10 +20,7 @@ export class FormComponent {
 
   onSubmit() {
     this.submitted = true;
-    this.user.name = this.form.value.username;
-    this.user.email = this.form.value.email;
-    this.user.password = this.form.value.password;
-    this.user.question = this.form.value.question;
+    this.user.name = this.form.value;
     setTimeout(()=>
       this.form.reset(),
       3000)
