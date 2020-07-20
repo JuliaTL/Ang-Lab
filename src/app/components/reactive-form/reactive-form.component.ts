3aea1c09345d31da7a3a2f0893ab64c2c51e3334
Julia <yulenkavip@gmail.com>
2020-07-20 19:21:44 +0300

    2,4,5

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-reactive-form',
@@ -8,20 +8,39 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class ReactiveFormComponent implements OnInit {
   signupForm: FormGroup;
+  submitted: boolean = false;
+  forbiddenName = 'John';
 
   constructor() { }
+
+  get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.checkForbiddenName.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
-      'question': new FormControl('car')
+      'question': new FormControl('car'),
+      'hobbies': new FormArray([])
     });
   }
 
   onSubmit() {
-    console.log(this.signupForm);
+    console.log(this.signupForm.value.userData.email);
+    this.submitted = true;
+  }
+  checkForbiddenName(control: FormControl): {[k: string]: boolean} {
+    if(this.forbiddenName.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true}
+    }
+    return null;
+  }
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    const formHobbies =  this.signupForm.get('hobbies') as FormArray;
+    formHobbies.push(control);
   }
 }
